1d0247fab9250411c46fe8d381a6af8322d2fd36
AdamPillis <adam.pillis@hotmail.com>
2022-02-03 10:24:34 +0000

    Add: form content to add_task.html and POST data to taskmanager

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,19 +2,22 @@ document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
-});
 
-document.addEventListener('DOMContentLoaded', function() {
+
     // delete modal initialization
     let modal = document.querySelectorAll('.modal');
     M.Modal.init(modal);
-  });
 
-  document.addEventListener('DOMContentLoaded', function() {
+
       // date picker initialization
     let datepicker = document.querySelectorAll('.datepicker');
     M.Datepicker.init(datepicker, {
         format: "dd mmm, yyyy",
         i18n: {done: "Select"}
     });
+
+
+    // select category type initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
